7e8ee637d1f5752697b1e218095873504e479358
projectdx <com114@gmail.com>
2024-04-02 18:15:31 +0900

    33

--- __init__.py
+++ __init__.py
@@ -1,70 +1,70 @@
 import os
 
-try:
-    import xmltodict
-except:
-    os.system("pip install xmltodict")
+# try:
+#     import xmltodict
+# except:
+#     os.system("pip install xmltodict")
 
-try:
-    import lxml
-except:
-    os.system("pip install lxml")
+# try:
+#     import lxml
+# except:
+#     os.system("pip install lxml")
 
-from support import SupportSC
+# from support import SupportSC
 
-from .site_util import SiteUtil
+# from .site_util import SiteUtil
 
-try:
-    if os.path.exists(os.path.join(os.path.dirname(__file__), "wavve.py")):
-        from .wavve import SupportWavve
-    else:
-        SupportWavve = SupportSC.load_module_f(__file__, "wavve").SupportWavve
-except:
-    pass
+# try:
+#     if os.path.exists(os.path.join(os.path.dirname(__file__), "wavve.py")):
+#         from .wavve import SupportWavve
+#     else:
+#         SupportWavve = SupportSC.load_module_f(__file__, "wavve").SupportWavve
+# except:
+#     pass
 
-try:
-    if os.path.exists(os.path.join(os.path.dirname(__file__), "kakaotv.py")):
-        from .kakaotv import SupportKakaotv
-    else:
-        SupportKakaotv = SupportSC.load_module_f(
-            __file__, "kakaotv"
-        ).SupportKakaotv
-except:
-    pass
+# try:
+#     if os.path.exists(os.path.join(os.path.dirname(__file__), "kakaotv.py")):
+#         from .kakaotv import SupportKakaotv
+#     else:
+#         SupportKakaotv = SupportSC.load_module_f(
+#             __file__, "kakaotv"
+#         ).SupportKakaotv
+# except:
+#     pass
 
-try:
-    if os.path.exists(os.path.join(os.path.dirname(__file__), "seezn.py")):
-        from .seezn import SupportSeezn
-    else:
-        SupportSeezn = SupportSC.load_module_f(__file__, "seezn").SupportSeezn
-except:
-    pass
+# try:
+#     if os.path.exists(os.path.join(os.path.dirname(__file__), "seezn.py")):
+#         from .seezn import SupportSeezn
+#     else:
+#         SupportSeezn = SupportSC.load_module_f(__file__, "seezn").SupportSeezn
+# except:
+#     pass
 
-try:
-    if os.path.exists(os.path.join(os.path.dirname(__file__), "tving.py")):
-        from .tving import SupportTving
-    else:
-        SupportTving = SupportSC.load_module_f(__file__, "tving").SupportTving
-except:
-    pass
-
-
-try:
-    if os.path.exists(os.path.join(os.path.dirname(__file__), "cppl.py")):
-        from .cppl import SupportCppl
-    else:
-        SupportCppl = SupportSC.load_module_f(__file__, "cppl").SupportCppl
-except:
-    pass
+# try:
+#     if os.path.exists(os.path.join(os.path.dirname(__file__), "tving.py")):
+#         from .tving import SupportTving
+#     else:
+#         SupportTving = SupportSC.load_module_f(__file__, "tving").SupportTving
+# except:
+#     pass
 
 
-try:
-    if os.path.exists(os.path.join(os.path.dirname(__file__), "dl_watcha.py")):
-        from .dl_watcha import DL_Watcha
-    else:
-        DL_Watcha = SupportSC.load_module_f(__file__, "dl_watcha").DL_Watcha
-except:
-    pass
+# try:
+#     if os.path.exists(os.path.join(os.path.dirname(__file__), "cppl.py")):
+#         from .cppl import SupportCppl
+#     else:
+#         SupportCppl = SupportSC.load_module_f(__file__, "cppl").SupportCppl
+# except:
+#     pass
+
+
+# try:
+#     if os.path.exists(os.path.join(os.path.dirname(__file__), "dl_watcha.py")):
+#         from .dl_watcha import DL_Watcha
+#     else:
+#         DL_Watcha = SupportSC.load_module_f(__file__, "dl_watcha").DL_Watcha
+# except:
+#     pass
 
 
 # from .server_util import MetadataServerUtil
